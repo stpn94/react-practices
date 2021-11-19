59c9e71677e15370b54a62ec825a4a1216a30e10
stpn94 <stpn94@gmail.com>
2021-11-19 16:56:00 +0900

    DOM API 기반전통적인 DOM API 기반의 애플리케이션

--- basic/ex01/public/index.js
+++ basic/ex01/public/index.js
@@ -1,7 +1,10 @@
 const App = function () {
+  //Dom API
+  //element가 component 이다.
   const app = document.createElement("h1");
   app.textContent = "Hello World";
   return app;
 };
 
+//Rendering 랜더링이라고 한다.
 document.getElementById("root").appendChild(App());
